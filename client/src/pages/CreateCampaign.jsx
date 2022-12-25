8f517166c561e28c146c2e423ae3d9af193abd79
Shin-Sae-byeok <toqur1219@naver.com>
2022-12-25 09:37:48 +0900

    feat: create project function and navigate home

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -3,7 +3,7 @@ import { useNavigate  } from 'react-router-dom';
 import { ethers } from 'ethers';
 
 import { useStateContext } from '../context';
-import { createCampaign, money } from '../assets';
+import { money } from '../assets';
 import { CustomButton, FormField } from '../components';
 import { checkIfImage } from '../utils';
 
@@ -12,8 +12,8 @@ import { checkIfImage } from '../utils';
 const CreateCampaign = () => {
 
   const navigate = useNavigate();
-  const [isLoading, setisLoading ] = useState(false);
-  const { CreateCampaign } = useStateContext();
+  const [isLoading, setIsLoading ] = useState(false);
+  const { createCampaign } = useStateContext();
   const [form, setForm] = useState({
     name: '',
     title: '',
@@ -30,9 +30,18 @@ const CreateCampaign = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
-    console.log(form);
-
+    checkIfImage(form.image, async(exists) => {
+      if(exists) {
+        setIsLoading(true)
+        await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
+        console.log(form);
+        setIsLoading(false);
+        navigate('/')
+      } else {
+        alert('유효하지 않은 이미지 url 입니다')
+        setForm({ ...form, image:""});
+      }
+    })
   }
 
   return (
